94cc722821edd0408b8d619e9c2307d330fc8eea
Valentyn <valentunpron@gmail.com>
2022-08-16 13:05:46 +0300

    complete work this block

--- js/script.js
+++ js/script.js
@@ -10,5 +10,28 @@ $(document).ready( () => {
 			}
 		};
 	})
-	
+
+	// ---- Modal ----
+	function hideModal() {
+		$('.benefist__body').each((inxed, item) => {
+			item.classList.remove('active');
+			$('.benefist__button')[inxed].classList.remove('benefist__button--active')
+		})
+	}
+	function showModal(i = 0) {
+		$('.benefist__body')[i].classList.add('active');
+		$('.benefist__button')[i].classList.add('benefist__button--active')
+	}
+	hideModal();
+	showModal();
+
+	$('.benefist__button').click(e => {
+		console.log(e.currentTarget);
+		$('.benefist__button').each((i, btn) => {
+			if(e.currentTarget == btn) {
+				hideModal();
+				showModal(i)
+			}
+		})
+	})
 })
